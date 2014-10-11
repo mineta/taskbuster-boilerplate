21aa356a809cad7d7c12f06bdcb5bc3dbd4904b0
mineta <marina.mele@gmail.com>
2014-10-11 17:23:08 +0200

    Update base.py

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.sites',
-    # 'django.contrib.sitemaps',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,7 +98,4 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, "templates"),
 )
-
-# Sites Framework
-# SITE_ID = 1
 
