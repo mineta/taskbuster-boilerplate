acee206e82aa8ae9e1b7422789d3e7e305052aff
Marina <marina.mele@gmail.com>
2015-05-10 16:32:29 +0200

    Upgrade to Django 1.8

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from .views import home, home_files
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
         home_files, name='home-files'),
     url(r'i18n/', include('django.conf.urls.i18n')),
-)
+]
 
 urlpatterns += i18n_patterns(
-    '',
     url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
